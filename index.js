52fc0b6a89ca634c346b971f74830f1d9ce60929
tuhin902 <tuhin902md@gmail.com>
2022-11-10 20:07:47 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 4000;
 
 //middle wares
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.hesma7h.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+
+}
+run().catch(console.dir)
 
 
 app.get('/', (req, res) => {
